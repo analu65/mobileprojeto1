9f411306211258d31a3d6d84913fd126eadfeb9c
analu65 <analuciacesario910@gmail.com>
2025-03-26 14:44:55 -0300

    navigator

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
-import Login from './screens/login';
 import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './screens/login';
 import Test from './screens/home';
-import { View, StyleSheet } from 'react-native';
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Test/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Login' component={Login}></Stack.Screen>
+        <Stack.Screen name='Home' component={Test}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
-const styles = StyleSheet.create({
 
-  container: {
-    flex: 1,
-
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-});
